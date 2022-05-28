f730a121b17362397e58f32a1f14c657c137f55e
Dave <davidsibanda776@gmail.com>
2022-05-28 22:14:46 +0200

    added default pagination

--- config/config/settings.py
+++ config/config/settings.py
@@ -62,9 +62,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'accounts.authenticate.Authentication',
     ),
-    # 'DEFAULT_PAGINATION_CLASS': (
-    #     'rest_framework.pagination.LimitOffsetPagination',
-    # ),
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
         ]
--- config/message/views.py
+++ config/message/views.py
@@ -3,7 +3,8 @@ from rest_framework import generics
 from .serializers import MessageSerializer
 from .models import Message
 
-# Create your views here.
+
+
 class MessageListView(generics.ListCreateAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
